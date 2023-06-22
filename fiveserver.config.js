065f2ce301649291580016b8f44691d210c7e25c
Jurgen Visser <jvisser31@noorderpoort.nl>
2023-06-22 15:09:29 +0200

    begin opdracht

---
+++ fiveserver.config.js
@@ -0,0 +1,11 @@
+// fiveserver.config.js
+module.exports = {
+  highlight: true, // enable highlight feature
+  injectBody: true, // enable instant update
+  remoteLogs: true, // enable remoteLogs
+  remoteLogs: "yellow", // enable remoteLogs and use the color yellow
+  injectCss: false, // disable injecting css
+  navigate: false, // disable auto-navigation
+  php: "/Applications/MAMP/bin/php/php7.4.12/bin/php",
+  // php: "/Applications/MAMP/bin/php/php8.0.0/bin/php"
+};
