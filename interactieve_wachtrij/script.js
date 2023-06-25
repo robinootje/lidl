be830dba51e27b07763958b3b7947b34a963bbdd
Jurgen Visser <jvisser31@noorderpoort.nl>
2023-06-25 19:33:29 +0200

    exercise finished

--- interactieve_wachtrij/script.js
+++ interactieve_wachtrij/script.js
@@ -2,24 +2,30 @@ let questionElement = document.getElementById("question");
 let countdownElement = document.getElementById("countdown");
 let answerElements = document.getElementsByClassName('answer');
 
-let questionsArray = ["What is this?","What is that?", "Who is you?","Where is me?","Something wrong?"];
-let answerArray = ["*A Quiz", "*Something","*A Person", "*Home", "*Nope"];
-let fakeAnswers = [["Breakfast","Potato"],["Nothing","Everything"],["A Dog","A Cat"],["Outside","Away"],["Yes","Always"]];
+//Alle vragen
+let questionsArray = ["What is this?","What is that?", "Who is you?","Where is me?","Something wrong?","wat vind je het lekkerst?","waarom niet?"];
+//Alle correcte antwoorden, als er geen antwoord is dan zet je "" neer. Dit moet omdat de antwoorden anders niet meer kloppen met de vraag.
+let answerArray = ["*A Quiz", "*Something","*A Person", "*Home", "*Nope","","*Daarom"];
+//Alle foute antwoorden (2 per vraag), als er geen antwoord is dan zet je [] neer. dit moet omdat de antwoorden anders niet meer kloppen met de vraag.
+let wrongAnswers = [["Breakfast","Potato"],["Nothing","Everything"],["A Dog","A Cat"],["Outside","Away"],["Yes","Always"],[],["Daarom niet","Gewoon"]];
+
+// lege let voor de antwoorden
 let currentAnswers = [];
 
+// Index van de vraag, deze begint op -1 omdat de eerste vraag pas na 5 seconden wordt laten zien.
 let index = -1;
 let maxIndex = questionsArray.length-1;
 
 // "timer" heeft nu een waarde van 5000 wat 5000 ms betekent, oftewel 5 seconden.
 // Mocht je dit willen aanpassen naar 10 seconden dan kan je 5000 naar 10000 veranderen.
 // Deze timer is alleen voor de eerste keer dat het inlaad. Je hebt dan nu 5 seconden voordat de eerste vraag wordt laten zien.
-let timer = setTimeout(setNextQuestion, 5000);
-
+let timer = setTimeout(setNextQuestion, 3000);
+// Synchroniseer dit nummer met de timer hierboven
 let counter = 5;
+// Zet de timer op de pagina
 countdownElement.innerHTML = counter;
-
+// Zet de eerste vraag op de pagina
 let interval = setInterval(setCountdown, 1000);
-
 Array.from(answerElements).forEach(element => {
     element.addEventListener("click",function() {
             handleAnswer(element);
@@ -27,15 +33,16 @@ Array.from(answerElements).forEach(element => {
     )
 });
 
-function setCountdown(){
+function setCountdown() {
+    // Als de timer op 0 staat, zet dan de volgende vraag klaar.
     counter--;
     countdownElement.innerHTML = counter;
 }
 
 
-function setNextQuestion(){
-    // If not out of questions set new question after 5 sec and set the timer again.
-    if(index !== maxIndex){
+function setNextQuestion() {
+    // Als er nog vragen zijn, laat dan de volgende vraag zien na 5 seconden en zet de timer weer aan.
+    if (index !== maxIndex) {
         clearInterval(interval);
         clearTimeout(timer);
         index++;
@@ -47,21 +54,34 @@ function setNextQuestion(){
         // Mocht je dit willen aanpassen naar 10 seconden dan kan je 5000 naar 10000 veranderen.
         timer = setTimeout(setNextQuestion,5000);
         interval = setInterval(setCountdown, 1000);
-    }else{
+    } else {
+        // Als er geen vragen meer zijn, laat dan een alert zien en refresh de pagina.
         if (!alert('De quiz is klaar')) { window.location.reload(); }
     }
 }
 
-function setAnswers(){
-    currentAnswers = [answerArray[index], fakeAnswers[index][0], fakeAnswers[index][1]];
-     shuffleArray(currentAnswers);
+function setAnswers() {
+    // Als er geen antwoord is, zet dan alle antwoorden op display:none;
+    if (answerArray[index] === "") {
+        for (var i = 0;i < answerElements.length;i++) {
+            answerElements[i].style = "Display:none;";  
+        }
+    } else {
+    // Als er wel een antwoord is, zet dan alle antwoorden op display:block;
 
-     for(var i = 0;i < answerElements.length;i++){
-        answerElements[i].innerHTML=currentAnswers[i];
-     }
+    // Zet de antwoorden in een array en shuffle deze.
+    currentAnswers = [answerArray[index], wrongAnswers[index][0], wrongAnswers[index][1]];
+        shuffleArray(currentAnswers);
+
+        for (var i = 0;i < answerElements.length;i++) {
+            answerElements[i].style = "";  
+            answerElements[i].innerHTML=currentAnswers[i];
+        }
+    }
 }
 
 function shuffleArray(array) {
+    // Deze functie zorgt ervoor dat de antwoorden random worden weergegeven.
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = array[i];
@@ -71,11 +91,11 @@ function shuffleArray(array) {
 }
 
 
-function handleAnswer(element){
-    // Correct answer before timer runs out. Clear the timer and set the next question
+function handleAnswer(element) {
+    // Corract antwoord voordat de timer op is. Stop de timer en zet de volgende vraag klaar.
     console.log(element.innerHTML);
     console.log(answerArray[index]);
-    if(element.innerHTML === answerArray[index]){
+    if (element.innerHTML === answerArray[index]) {
         element.classList.add("correct-answer");
         setNextQuestion();
         element.classList.remove("correct-answer");
